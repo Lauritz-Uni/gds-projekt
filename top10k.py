d54eb0334bdbfb01ea88412ee79ed2ed1afab2eb
Lauritz-Uni <fqm630@alumni.ku.dk>
2025-03-06 14:09:07 +0100

    top10k^2

--- top10k.py
+++ top10k.py
@@ -9,14 +9,16 @@ def wordFrequency(csv_data, content_column):
         counter.update(document)
     return counter
 
-start_time = time.time()
+def get_top_words(filepath: str = 'data/news_sample.csv', content: str = 'content'):
+    processor = cpp.TextProcessor()
+    csv_data = processor.full_pipeline(filepath, 'content')
 
-processor = cpp.TextProcessor()
-csv_data = processor.full_pipeline('data/news_sample.csv', 'content')
+    counter = wordFrequency(csv_data, content+'-tokens_no_stop')
 
-counter = wordFrequency(csv_data, 'content-tokens_no_stop')
-
-top_words = [word for word, _ in counter.most_common(10000)]
-print(top_words)
-
-print("--- %s seconds ---" % (time.time() - start_time))
+    top_words = [word for word, _ in counter.most_common(10000)]
+    return top_words
+    
+if __name__ == "__main__":
+    start_time = time.time()
+    print(get_top_words()[:20])
+    print(f"Time taken to get top words: {time.time() - start_time:.2f}s")
