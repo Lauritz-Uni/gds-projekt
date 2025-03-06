c0d534a21696e510f30e4c93d588ba51e0a320f6
theodorff <theodorflood@gmail.com>
2025-03-06 14:09:13 +0100

    Fake/reliable word count done :)

--- fake_reliable_word_count.py
+++ fake_reliable_word_count.py
@@ -1,10 +1,11 @@
 import code_pre_processor
 import pandas as pd
 from collections import Counter
+import top10k
 
 
 # List of 10.000 most frequent words in all texts
-word_list = {"word", "list"}                                                                      #put list here
+word_list = top10k.top_words                                                                      #put list here
 
 # Reading csv files
 fake_csv_file = code_pre_processor.read_csv_file("fake_articles_sample.csv")            #replace path with fake text path       
@@ -13,14 +14,18 @@ reliable_csv_file = code_pre_processor.read_csv_file("reliable_articles_sample.c
 
 
 
-fake_words = fake_csv_file.split()
-reliable_words = reliable_csv_file.split()
+fake_words = " ".join(fake_csv_file["content"]).split()
+reliable_words = " ".join(reliable_csv_file["content"]).split()
 
 fake_word_counts = Counter(word for word in fake_words if word in word_list)
 reliable_word_counts = Counter(word for word in reliable_words if word in word_list)
 
+
+
+print("Fake word counts")
 for word, count in fake_word_counts.items():
     print(f"{word}: {count}")
-
+    
+print("Reliable word counts")
 for word, count in reliable_word_counts.items():
     print(f"{word}: {count}")
