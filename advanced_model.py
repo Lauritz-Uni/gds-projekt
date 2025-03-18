53acaa16da2fd67a305f7fc793879cf20ae8e001
klara <klarabast@outlook.com>
2025-03-18 19:52:42 +0100

    Updated Advanced model with accuracy score.

--- advanced_model.py
+++ advanced_model.py
@@ -6,8 +6,10 @@ import joblib
 from utils import get_top_words, categorize_reliable_or_fake
 
 #using the 10.000 list
-csv_data = cpp.read_csv_file('data/news_sample_processed.csv')
-top_words = [get_top_words(csv_data)[:10000]]
+csv_data_train = cpp.read_csv_file('data/995,000_rows_processed_train.csv')
+csv_data_test = read_csv_file('data/995,000_rows_processed_test.csv')
+csv_data_val = read_csv_file('data/995,000_rows_processed_val.csv')
+top_words = [get_top_words(csv_data_train)[:10000]]
 
 #convert preprocessed text into TF-IDF weights/features, using the top 10.000 words
 vectorizer = TfidfVectorizer(vocabulary=top_words) #No need for: stop_words='english' because data has been preprocessed already..
@@ -16,7 +18,7 @@ tfidf_test = vectorizer.transform("testing_data") #just transforming the testing
 tfidf_val = vectorizer.transform("validation_data") #transforming the validation data split part into the same feature space (TF-IDF)
 
 #applying the categorize_reliable_or_fake function to the type column of the csv_data to extract all labels
-csv_data["binary_type"] = csv_data["type"].apply(categorize_reliable_or_fake)
+csv_data_train["binary_type"] = csv_data["type"].apply(categorize_reliable_or_fake)
 
 training_labels = csv_data_train["binary_type"].values
 test_labels = csv_data_test["binary_type"].values
@@ -26,3 +28,13 @@ val_labels = csv_data_val["binary_type"].values
 #training the naive bayes classifier model!
 model = ComplementNB() #Naive bayes classifier, multinomial version means based on word frequency counts hence TF-IDF scores
 model.fit(tfidf_train, "training_labels") #where tfidk_train is the transformed training set, and "training labels"
+
+
+#testing model accuracy
+test_pred = model.predict(tfidf_test)
+train_pred = model.predict(tfidf_train)
+
+
+print(f"Model Accuracy for training data: {accuracy_score("training_labels", train_pred)*100} %\n")
+print(f"Model Accuracy for test data: {accuracy_score("test_labels", test_pred)*100} % \n\n")
+print(f"Model Report: \n\n {classification_report("test_labels", test_pred)}")
